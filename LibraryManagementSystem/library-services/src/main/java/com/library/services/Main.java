783ab199ce95bfae384270875c153627c8b9bd40
Jen Russell <jennifer@jrussellux.com>
2024-10-20 14:11:31 -0700

    Multi-module poms

--- LibraryManagementSystem/library-services/src/main/java/com/library/services/Main.java
+++ LibraryManagementSystem/library-services/src/main/java/com/library/services/Main.java
@@ -1,7 +1,9 @@
 package com.library.services;
+import com.library.core.Book;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Book book = new Book();
+        book.addBook("My Title");
     }
 }
