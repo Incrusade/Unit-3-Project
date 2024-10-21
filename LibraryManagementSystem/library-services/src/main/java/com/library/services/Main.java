5ad2bebba5735cd683cec1f67197697bed2646fe
Jen Russell <jennifer@jrussellux.com>
2024-10-21 15:18:25 -0700

    Add unit tests

--- LibraryManagementSystem/library-services/src/main/java/com/library/services/Main.java
+++ LibraryManagementSystem/library-services/src/main/java/com/library/services/Main.java
@@ -4,7 +4,11 @@ import com.library.core.Member;
 
 public class Main {
     public static void main(String[] args) {
-        Book.addBook("My Title", "My Author");
-        Member.addMember("Matt");
+        Book.addBook("My Title", "My Author", "12345");
+        Book.addBook("Mary had a little lamb", "You", "23456");
+
+        Book.listBooks();
+
+        //Member.addMember("Matt");
     }
 }
