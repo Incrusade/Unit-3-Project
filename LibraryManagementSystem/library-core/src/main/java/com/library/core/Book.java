6a56f729243e6fd215f4aa69b8724315bba79fab
Jen Russell <jennifer@jrussellux.com>
2024-10-22 15:52:08 -0700

    removed code

--- LibraryManagementSystem/library-core/src/main/java/com/library/core/Book.java
+++ LibraryManagementSystem/library-core/src/main/java/com/library/core/Book.java
@@ -1,44 +1,11 @@
 package com.library.core;
 
-import java.util.ArrayList;
-
 public class Book {
-    private String title;
-    private String author;
-    private String isbn;
-
-    private static ArrayList<Book> books = new ArrayList<Book>();
-
-    public static void addBook(String title, String author, String isbn) {
-        Book book = new Book();
-        book.title = title;
-        book.author = author;
-        book.isbn = isbn;
-        books.add(book);
-        System.out.printf("Added book, now have %d books\n", books.size());
-    }
-
-    public static void listBooks() {
-        for (int i = 0; i < books.size(); i++) {
-            Book book = books.get(i);
-            System.out.printf("Book %d: %s (%s) %s\n", i, book.title, book.author, book.isbn);
-        }
+    public static void addBook(String title, String author) {
+        System.out.println("Added book");
     }
 
     public static void removeBook(String title) {
-        for (int i = 0; i < books.size(); i++) {
-            Book book = books.get(i);
-            if (book.title == title) {
-                // Found book
-                books.remove(i);
-                System.out.println("Removed book");
-                return;
-            }
-        }
-        System.out.println("Cannot find book");
-    }
-
-    public static int bookCount() {
-        return books.size();
+        System.out.println("Book removed");
     }
 }
--- LibraryManagementSystem/library-services/src/main/java/com/library/services/Main.java
+++ LibraryManagementSystem/library-services/src/main/java/com/library/services/Main.java
@@ -1,14 +1,8 @@
 package com.library.services;
 import com.library.core.Book;
-import com.library.core.Member;
 
 public class Main {
     public static void main(String[] args) {
-        Book.addBook("My Title", "My Author", "12345");
-        Book.addBook("Mary had a little lamb", "You", "23456");
-
-        Book.listBooks();
-
-        //Member.addMember("Matt");
+        Book.addBook("My Title", "My Author");
     }
 }
